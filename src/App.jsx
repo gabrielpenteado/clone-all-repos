3f0f254be9cddad6157d40fd63e5413a8e948db6
Gabriel Penteado <gabripenteado@gmail.com>
2025-02-25 01:55:06 -0300

    feat: add tooltip icon and modify downloading animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import axios from "axios";
+import { FaDownload, FaFolderOpen, FaInfoCircle } from "react-icons/fa";
 
 const App = () => {
   const [username, setUsername] = useState("");
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState("");
+  const [isHovered, setIsHovered] = useState(false);
 
   const handleUsernameChange = (e) => {
     setUsername(e.target.value);
@@ -20,53 +22,97 @@ const App = () => {
     setLoading(true);
 
     try {
-      // Sending the username to the backend
       const response = await axios.post(
         "http://localhost:3000",
         { user: username },
         { responseType: "blob" }
       );
 
-      // Create a temporary link and trigger the download
       const url = window.URL.createObjectURL(new Blob([response.data]));
       const a = document.createElement("a");
       a.href = url;
-      a.setAttribute("download", `${username}_repos.zip`); // Name of the file to download
+      a.setAttribute("download", `${username}_repos.zip`);
       document.body.appendChild(a);
       a.click();
 
-      // Reset loading state
-      setLoading(false);
+      setUsername("");
     } catch (err) {
       console.error(err);
       setError("Error downloading repositories.");
+    } finally {
       setLoading(false);
     }
   };
 
   return (
-    <div className="flex items-center justify-center h-screen bg-gray-100">
-      <div className="bg-white p-6 rounded-lg shadow-md w-96">
-        <h1 className="text-2xl font-bold mb-4">
-          GitHub Repositories Downloader
+    <div className="flex items-center justify-center h-screen bg-gradient-to-br from-indigo-900 to-blue-800">
+      <div className="bg-white p-8 rounded-2xl shadow-xl w-96 transition-all duration-300 hover:shadow-2xl relative">
+        <h1 className="text-2xl font-bold text-center mb-8 text-blue-600">
+          GitHub Repo Downloader
         </h1>
-        <input
-          type="text"
-          value={username}
-          onChange={handleUsernameChange}
-          placeholder="Enter GitHub username"
-          className="w-full p-3 border border-gray-300 rounded mb-4"
-        />
+
+        <div className="absolute top-2 left-2 md:top-2 md:right-2">
+          <FaInfoCircle
+            className="w-5 h-5 text-blue-500 cursor-pointer"
+            onMouseEnter={() => setIsHovered(true)}
+            onMouseLeave={() => setIsHovered(false)}
+          />
+
+          {isHovered && (
+            <div className="absolute transform -translate-y-8 left-0 -top-18 bg-white shadow-md rounded-lg p-3 w-72 text-[12px] md:text-sm text-gray-700 border">
+              <div className="flex items-start space-x-2 mb-2">
+                <span className="text-blue-500">•</span>
+                <span>Works only with public repositories.</span>
+              </div>
+              <div className="flex items-start space-x-2">
+                <span className="text-blue-500">•</span>
+                <span>Has a limit of 100 repositories.</span>
+              </div>
+            </div>
+          )}
+        </div>
+
+        <div className="mb-6 relative">
+          <input
+            type="text"
+            value={username}
+            onChange={handleUsernameChange}
+            placeholder="Enter GitHub username"
+            className="w-full p-4 border-2 border-gray-200 rounded-xl focus:outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-200 transition-all"
+          />
+        </div>
+
+        {loading && (
+          <div className="flex flex-col items-center mb-6">
+            <div className="relative animate-pulse">
+              <FaFolderOpen className="text-6xl text-blue-500" />
+            </div>
+            <p className="mt-4 text-gray-600 font-medium">
+              Cloning repositories...
+            </p>
+          </div>
+        )}
+
         <button
           onClick={handleSubmit}
           disabled={loading}
-          className={`w-full p-3 text-white rounded ${
-            loading ? "bg-gray-500" : "bg-blue-500"
-          } hover:bg-blue-600`}
+          className={`w-full cursor-pointer py-4 px-6 rounded-xl font-semibold text-white flex items-center justify-center gap-2 transition-all ${
+            loading ? "hidden" : "bg-blue-500 hover:bg-blue-600"
+          }`}
         >
-          {loading ? "Cloning..." : "Download Repos as ZIP"}
+          {!loading && (
+            <div className="flex items-center gap-4">
+              <FaDownload />
+              <span>Download ZIP</span>
+            </div>
+          )}
         </button>
-        {error && <p className="text-red-500 mt-4">{error}</p>}
+
+        {error && (
+          <p className="mt-4 p-3 text-red-600 bg-red-50 rounded-lg text-center animate-shake">
+            {error}
+          </p>
+        )}
       </div>
     </div>
   );
